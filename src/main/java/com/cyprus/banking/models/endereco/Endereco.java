f7e792726582208db328edae4f40fa670e694222
anac-meneses <ana.meneses@live.com>
2023-03-09 17:18:53 -0300

    gerenteService updated

--- src/main/java/com/cyprus/banking/models/endereco/Endereco.java
+++ src/main/java/com/cyprus/banking/models/endereco/Endereco.java
@@ -6,7 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Endereco {
 	@Id
--- src/main/java/com/cyprus/banking/models/gerente/servicos/GerenteServico.java
+++ src/main/java/com/cyprus/banking/models/gerente/servicos/GerenteServico.java
@@ -1,5 +1,25 @@
 package com.cyprus.banking.models.gerente.servicos;
 
+import org.springframework.stereotype.Service;
+
+import com.cyprus.banking.models.conta.Conta;
+
+@Service
 public class GerenteServico {
+	private ContaRepository contaRepository;
+	private ClienteRepository clienteRepository;
+	private DependenteRepository dependenteRepository;
+
+	public Conta alterarDadosDaConta(Conta conta) {
+//		preciso pensar nessa logica
+		contaRepository.save(conta);
+		return conta;
+	}
+
+	public void excluirCliente() {
+//		preciso pensar nessa logica
+	Cliente cliente	= clienteRepository.findByEmail(cliente.getEmail);
+	clienteRepository.deleteById(cliente.getId_cliente);
+	}
 
 }
