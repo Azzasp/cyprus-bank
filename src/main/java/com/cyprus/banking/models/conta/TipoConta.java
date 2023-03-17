706dc68e31ea42397b3f02993484c4be4e6cc7b7
Caroline Coser <87380522+carolcoser@users.noreply.github.com>
2023-03-17 13:35:54 -0300

    Add models Conta and TipoConta

---
+++ src/main/java/com/cyprus/banking/models/conta/TipoConta.java
@@ -0,0 +1,30 @@
+package com.cyprus.banking.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+
+@Entity
+@Table(name = "tipoConta")
+@Getter
+@Setter
+public class TipoConta {
+
+@Id
+@SequenceGenerator(name = "id_tipoConta", allocationSize = 1, sequenceName = "id_sequence")
+@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
+	private int id_tipoConta;
+	private String descricao;
+	
+	public TipoConta() {
+	}
+	
+	public TipoConta(int id_tipoConta, String descricao) {
+		super();
+		this.id_tipoConta = id_tipoConta;
+		this.descricao = descricao;
+	}
+}
+
+
